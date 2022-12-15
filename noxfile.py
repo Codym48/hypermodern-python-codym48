3b714ca3c65cdeba6c540782ab2d15921b87b5b9
Cody Martin <codytylermartin@gmail.com>
2022-12-15 13:47:47 -0700

    Lint with flake8, format with black

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,22 @@
 import nox
 
+locations = "src", "tests", "noxfile.py"
+nox.options.sessions = "lint", "tests"
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black")
+    session.run("flake8", *args)
+
 
 @nox.session(python=["3.11", "3.10", "3.8"])
 def tests(session):
